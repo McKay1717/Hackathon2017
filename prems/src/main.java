ab68d7fa19dc79b0e742c134e3befde6df89060e
LaroyenneG <laroyenneg@gmail.com>
2017-01-20 10:27:04 +0100

    super non premier

--- prems/src/main.java
+++ prems/src/main.java
@@ -8,27 +8,125 @@ import java.util.*;
    judicieux de générer la liste des nombres premiers de 2 jusqu'à lim.
  */
 class Checker {
-    int lim; // la valeur max. des nombres à traiter
-
-    // autres attributs
+    private int lim; // la valeur max. des nombres à traiter
+    private List<Integer> listNumber;
 
     public Checker(int lim) {
         this.lim = lim;
-        // init. autres attributs
+        listNumber=new ArrayList<>();
+
+        createList();
     }
 
-    public String check(int value) {
-        boolean good=true;
-        // retourne "yes" su value est un super non-premier
-        // "false" sinon
-        if(good){
-            return "yes";
-        }else {
-            return "false";
+    private void createList(){
+        for (int n=10;n<=lim; n++){
+            if(isSuperNonPremier(n)){
+                listNumber.add(n);
+            }
         }
     }
 
-    // autres méthodes.
+    static boolean isSuperNonPremier(int number){
+
+        if(!(number>=10)){
+            return false;
+        }
+
+        if(isPrime(number)){
+            return false;
+        }
+
+        if(!goodDigit(number)){
+            return false;
+        }
+
+        return true;
+    }
+
+    static boolean isPrime(int number){
+        if(number<2){
+            return false;
+        }
+        for (int i=2; i<=Math.sqrt(number); i++){
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int getIntInString(String string, int i){
+        return Character.getNumericValue(string.charAt(i));
+    }
+
+    static boolean goodDigit(int number){
+        String string = String.valueOf(number);
+
+        /*
+        check the digit one for one
+         */
+        for (int i=0; i<string.length(); i++){
+            int digit = getIntInString(string, i);
+            if(digit!=1 && !isPrime(digit)){
+                return false;
+            }
+        }
+
+        if(string.length()<=2){
+            return true;
+        }
+
+        /*
+        check 2 digit
+        */
+
+        for (int i=0; i<string.length()-1; i++){
+            int combi = 0;
+            combi+=getIntInString(string, i)*10;
+            combi+=getIntInString(string, i+1);
+            if(!isPrime(combi)){
+                return false;
+            }
+        }
+
+
+        /*
+        check 3 digit
+        */
+        for (int i=0; i<string.length()-2; i++){
+            int combi = 0;
+            combi+=getIntInString(string, i)*100;
+            combi+=getIntInString(string, i+1)*10;
+            combi+=getIntInString(string, i+2);
+            if(!isPrime(combi)){
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
+    public String check(int value) {
+        boolean good=false;
+
+        for (int i=0; i<listNumber.size(); i++){
+            if(listNumber.get(i)==value){
+                good=true;
+                break;
+            }
+        }
+
+        if(good){
+            return "yes";
+        }else {
+            return "no";
+        }
+    }
+
+    public String toString(){
+        return listNumber.toString();
+    }
 }
 
 
@@ -38,6 +136,7 @@ class SuperNonPremier {
         Locale.setDefault(Locale.ENGLISH);
 
         Checker c = new Checker(100000); // check for number till 100000
+        //System.out.println(c);
         Scanner scan = new Scanner(System.in);
         int nbCases = scan.nextInt(); // get numbers of cases from stdin
 
